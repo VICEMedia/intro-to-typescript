ff45a661cbd2693f6ecf1f4cbbd60fc962804c60
Paul Fielek <paul.fielek@vice.com>
2018-10-22 18:18:25 -0400

    Updated interfaces example

--- examples/interfaces.ts
+++ examples/interfaces.ts
@@ -3,9 +3,10 @@
   need to iterate over enum keys or when you need to reference an enum key
   at runtime
 */
-enum Power {
-  Transform = 'assume multiple forms',
-  SuckBlood = 'suck the life juice out of a victim',
+enum Weakness {
+  Sunlight = 'whithers in direct sunlight',
+  Decapitation = 'must be attached to their noggin',
+  Stake = 'is vulnerable to attacks from this pointed implement',
 }
 
 /*
@@ -14,60 +15,71 @@ enum Power {
 */
 type Media = 'novel' | 'film';
 
+interface BloodSucker {
+  pintsConsumed: number;
+}
+
+interface BrainEater {
+  brainsEaten: number;
+}
+
+interface Undead {
+  dateOfUndeath: Date;
+}
+
 interface Monster {
   name: string;
-  year: number;
-  media: Media;
-  powers: Power[];
-  listPowers: () => string;
+  hunger: number;
+  mediaOfOrigin: Media;
+  weaknesses: Weakness[];
+  consume: (number) => void;
 }
 
-class MovieMonster implements Monster {
+class Vampire implements Monster, Undead, BloodSucker {
   name: string;
-  year: number;
-  media: Media = 'film';
-  powers: Power[];
+  hunger = 100;
+  pintsConsumed = 0;
+  mediaOfOrigin: Media = 'novel';
+  weaknesses = [Weakness.Sunlight, Weakness.Decapitation, Weakness.Stake];
+  dateOfUndeath: Date;
 
-  constructor(name: string, year: number, powers: Power[]) {
+  constructor(name: string, dateOfUndeath: Date) {
     this.name = name;
-    this.year = year;
-    this.powers = powers;
+    this.dateOfUndeath = dateOfUndeath;
   }
 
-  listPowers() {
-    const { name, powers } = this;
-    return `${name} can ${powers.join(', ')}`;
+  consume(pints: number) {
+    this.hunger -= pints * 10;
+    this.pintsConsumed -= pints;
   }
 }
 
-const mogwai = new MovieMonster(
-  'Gizmo',
-  1984,
-  [Power.Transform]
-);
-
-class NovelMonster implements Monster {
+class Zombie implements Monster, Undead, BrainEater {
   name: string;
-  year: number;
-  media: Media = 'novel';
-  powers: Power[];
+  hunger = 50;
+  brainsEaten = 0;
+  mediaOfOrigin: Media = 'film';
+  weaknesses = [Weakness.Decapitation];
+  dateOfUndeath: Date;
 
-  constructor(name: string, year: number, powers: Power[]) {
+  constructor(name: string, dateOfUndeath: Date) {
     this.name = name;
-    this.year = year;
-    this.powers = powers;
+    this.dateOfUndeath = dateOfUndeath;
   }
 
-  listPowers() {
-    const { name, powers } = this;
-    return `${name} can ${powers.join(', ')}`;
+  consume(brains: number) {
+    this.hunger -= brains * 25;
+    this.brainsEaten += brains;
   }
 }
 
-const vampire = new NovelMonster(
-  'Dracula',
-  1897,
-  [Power.Transform, Power.SuckBlood]
-)
+const attack = (monster: Monster, weakness: Weakness) => {
+  if (monster.weaknesses.includes(weakness)) {
+    console.log(`${monster.name} ${weakness}`);
+  } else {
+    console.log(`${monster.name} survived the attack`);
+  }
+}
 
-console.log(vampire.listPowers());
+const dracula = new Vampire('Dracula', new Date('January 15 2018'));
+attack(dracula, Weakness.Decapitation);
