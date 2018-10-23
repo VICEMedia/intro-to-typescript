de7067dd2001786095c43a936fe847fd09faad0c
Charles Dimaggio <cj.dimaggio@vice.com>
2018-10-23 15:48:05 -0400

    Rig database

--- examples/monsters-api/src/interfaces/Monster.d.ts
+++ examples/monsters-api/src/interfaces/Monster.d.ts
@@ -2,5 +2,5 @@ import * as mongoose from 'mongoose';
 
 export default interface Monster {
   name: string;
-  save: (db: mongoose.Connection, args: any) => Promise<mongoose.Document>;
+  save: () => Promise<mongoose.Document>;
 }
